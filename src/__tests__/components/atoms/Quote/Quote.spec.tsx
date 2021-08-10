28acc79fc09d1e793f1150805b5a8f79ac72147d
Alexandre Menezes <amenezes031@gmail.com>
2021-08-10 14:42:52 -0300

    test: Quote.tsx test component

---
+++ src/__tests__/components/atoms/Quote/Quote.spec.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+
+import { Quote } from "../../../../components/atoms/Quote";
+
+describe("Testing atom Quote.tsx component", () => {
+  it("should be able to show the paragraph with some text", () => {
+    const childrenQuote = "My quote";
+    render(<Quote>{childrenQuote}</Quote>);
+
+    const textQuoteEl = screen.getByText(childrenQuote);
+
+    expect(textQuoteEl).toBeInTheDocument();
+  });
+});
