af749d0eed0e5c55701df8449d1add953e29dab9
Alexandre Menezes <amenezes031@gmail.com>
2021-08-16 13:40:11 -0300

    request quote api

--- src/services/LoadQuote.ts
+++ src/services/LoadQuote.ts
@@ -2,15 +2,28 @@ import { api } from "./api";
 
 type QuoteType = {
   quote: string;
-  author: string;
+  speaker: string;
 };
 
+interface QuoteResponse {
+  quote: string;
+  author: string;
+}
+
 export async function getQuote(): Promise<QuoteType> {
-  const { data } = await api(process.env.REACT_APP_API_URL || "");
-  const { quote, author }: QuoteType = data.contents.quotes[0];
+  const { data } = await api(process.env.REACT_APP_API_URL || "", {
+    params: {
+      language: "en",
+      limit: 1,
+    },
+    headers: {
+      accept: "application/json",
+    },
+  });
+  const { quote, author: speaker }: QuoteResponse = data.contents.quotes[0];
 
   return {
     quote,
-    author,
+    speaker,
   };
 }
