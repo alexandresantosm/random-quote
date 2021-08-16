527ecb470601795eaf56592562d9ce1cd62cfb7a
Alexandre Menezes <amenezes031@gmail.com>
2021-08-16 13:40:56 -0300

    show quote

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 import { NewQuoteButton } from "../../components/atoms/NewQuoteButton";
 import { ContentQuote } from "../../components/molecules/ContentQuote";
+import { getQuote } from "../../services/LoadQuote";
 
 import { Container, Content } from "./styles";
 
 export const Home = (): JSX.Element => {
+  const initalState = {
+    quote: "Never Stop Learning",
+    speaker: "Autor desconhecido",
+  };
+
+  const [quote, setQuote] = useState(initalState);
+
+  async function handleNewQuote() {
+    const { quote, speaker } = await getQuote();
+
+    setQuote({ quote, speaker });
+    console.log(quote);
+  }
+
   return (
     <Container>
       <Content>
-        <ContentQuote
-          quote="Never Stop Learning"
-          speaker="Autor desconhecido"
-        />
-        <NewQuoteButton>Nova citação</NewQuoteButton>
+        <ContentQuote {...quote} />
+        <NewQuoteButton onClick={() => handleNewQuote()}>
+          Nova citação
+        </NewQuoteButton>
       </Content>
     </Container>
   );
